31f702bfc1aacbd53e37c555e9c242b615deb98d
longyangking <longyang_123@yeah.net>
2018-08-07 23:00:28 +0800

    update readme

--- Parrot/main.c
+++ Parrot/main.c
@@ -1,9 +1,48 @@
 #include "phrase.h"
 #include "stdio.h"
+#include "string.h"
+#include "unistd.h"
+#include "getopt.h"
+
+char *__help_info__ = "Parrot help information";
 
 int main(int argc, char *argv[]){
-    if (argc > 2) {
-        printf("Error: Too many arguments!\n");
-        return;
-    }
+    int opt;
+    char *optstring = "f:hv";
+    const struct option long_options[] = {
+        {"filename", required_argument, NULL, 'f'},
+        {"help", no_argument, NULL, 'h'},
+        {"verbose", no_argument, NULL, 'v'},
+        {NULL, 0, NULL, 0}
+    };
+
+    char *filename;
+    int verbose = 0;
+    while((opt = getopt_long(argc, argv, optstring, long_options, NULL))!=-1){
+        switch(opt){
+            case 0: break;
+            case 'h': 
+                printf("%s\n",__help_info__);
+                break;
+            case 'f': 
+                filename = optarg;
+                break;
+            case 'v': 
+                verbose = 1;
+                break;
+            default:
+                printf("Unknown parameters: %s\n", opt);
+                return 0;
+        }
+    };
+
+    if (verbose) {
+        printf("Filename: %s\n", filename);
+    };
+
+    return 0;
+    // if (argc > 2) {
+    //     printf("Error: Too many arguments!\n");
+    //     return;
+    // }
 }
--- README.md
+++ README.md
@@ -5,3 +5,6 @@
 ## Features
 + Experimental language features
 + System interface supported
+
+## Status
+Unfortunately, I'm reviewing C currently and it may spend some time.
